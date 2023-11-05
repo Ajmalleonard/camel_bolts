bb4c7c1d417822cee59a4b7ba5ee52eca71f37b8
ajmalleonard <ajmalmaker@icloud.com>
2023-11-05 17:44:04 +0300

    Updated some resources

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:camel_bolt/firebase_options.dart';
+import 'package:camel_bolt/pages/startingPage/LoginPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:camel_bolt/pages/startingPage/LoginPage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const WhiteTrip());
+  runApp(const CamelBolt());
 }
 
-class WhiteTrip extends StatelessWidget {
-  const WhiteTrip({super.key});
+class CamelBolt extends StatelessWidget {
+  const CamelBolt({super.key});
 
   @override
   Widget build(BuildContext context) {
